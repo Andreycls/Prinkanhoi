32fed18e0344e446c5ea941d4658607eae2ae0fd
Andrey Cls <Andrey_Andrzz@rocketmail.com>
2018-01-23 11:25:38 +0700

    Add files via upload

---
+++ frontend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'kGAR1qP_X3cpgglgoJ3ErMfU3X_r3DMJ',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
